44ec6ca701fe30dce7cfb7b87b2207d08e107a8e
ymerinor <150065426+ymerinor@users.noreply.github.com>
2023-11-20 16:12:58 -0500

    Servicio de aplicaciones

---
+++ UserManagement.Application/Interface/IServiceBase.cs
@@ -0,0 +1,8 @@
+﻿using UserManagement.Domain.Repository.Interface;
+
+namespace UserManagement.Application.Interface
+{
+    internal interface IServiceBase<TEntidad, TEntidadID> : IRepositoryBase<TEntidad, TEntidadID>, IList<TEntidad, TEntidadID>
+    {
+    }
+}
